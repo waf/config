bae77fb616c836ea68d1c1dfaf532c2888264a26
Will Fuqua <wafuqua@gmail.com>
2020-08-16 11:05:01 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,44 +1,36 @@
-Set-PSReadlineOption -BellStyle None
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-# use Vi bindings, but selectively re-enable some others like copy/paste
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key Ctrl+a -Function SelectAll
-Set-PSReadLineKeyHandler -Key Ctrl+c -Function CopyOrCancelLine
-Set-PSReadLineKeyHandler -Key Ctrl+x -Function Cut
-Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
-Set-PSReadLineKeyHandler -Key Shift+LeftArrow -Function SelectBackwardChar
-Set-PSReadLineKeyHandler -Key Shift+RightArrow -Function SelectForwardChar
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+    Set-PSReadlineOption -BellStyle None
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    
+    # use vi bindings, but selectively re-enable some others like copy/paste
+    Set-PSReadLineOption -EditMode Vi
+    Set-PSReadLineKeyHandler -Key Ctrl+a -Function SelectAll
+    Set-PSReadLineKeyHandler -Key Ctrl+c -Function CopyOrCancelLine
+    Set-PSReadLineKeyHandler -Key Ctrl+x -Function Cut
+    Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
+    Set-PSReadLineKeyHandler -Key Shift+LeftArrow -Function SelectBackwardChar
+    Set-PSReadLineKeyHandler -Key Shift+RightArrow -Function SelectForwardChar
+    
+    # Dracula colors for PSReadLine -- works in conhost and wt -- install colors from https://github.com/dracula/powershell
+    Set-PSReadlineOption -Color @{
+        "Command" = [ConsoleColor]::Green
+        "Parameter" = [ConsoleColor]::Gray
+        "Operator" = [ConsoleColor]::Magenta
+        "Variable" = [ConsoleColor]::White
+        "String" = [ConsoleColor]::Yellow
+        "Number" = [ConsoleColor]::Blue
+        "Type" = [ConsoleColor]::Cyan
+        "Comment" = [ConsoleColor]::DarkCyan
+    }
 
-# Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
-Set-PSReadlineOption -Color @{
-    "Command" = [ConsoleColor]::Green
-    "Parameter" = [ConsoleColor]::Gray
-    "Operator" = [ConsoleColor]::Magenta
-    "Variable" = [ConsoleColor]::White
-    "String" = [ConsoleColor]::Yellow
-    "Number" = [ConsoleColor]::Blue
-    "Type" = [ConsoleColor]::Cyan
-    "Comment" = [ConsoleColor]::DarkCyan
+    Import-Module oh-my-posh
+    $DefaultUser = 'wafuqua' # hide "username@host" when on localhost and this user.
+    Set-Prompt
+
+    function ls_alias { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
+    Set-Alias -Name ls -Value ls_alias -Option Private
 }
-# Dracula Prompt Configuration
-Import-Module posh-git
-$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) "
-$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
-$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
-$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) "
-$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
-# Dracula Git Status Configuration
-$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
-$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
-$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-Import-Module oh-my-posh
-$DefaultUser = 'wafuqua'
-Set-Prompt
