7656532e005958c2c122194d1ebcd69831898692
Will Fuqua <wafuqua@gmail.com>
2021-08-01 14:46:50 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,12 @@
-# Module installation:
-# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force
+# Module installations (required only one time)
+# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 # Install-Module -Name posh-git -Scope CurrentUser
 # Install-Module -Name oh-my-posh -Scope CurrentUser
-# Install-Module -Name z -Scope CurrentUser
+# Install-Module -Name z -Scope CurrentUser -AllowClobber
 
-if ($host.Name -eq 'ConsoleHost')
+if ($host.Name -eq 'ConsoleHost') # only apply to terminals, don't apply to e.g. ISE.
 {
     Import-Module PSReadLine
-    Set-PSReadlineOption -BellStyle None
-    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
     
     # use vi bindings, but selectively re-enable some others like copy/paste
     Set-PSReadLineOption -EditMode Vi
@@ -22,25 +18,50 @@ if ($host.Name -eq 'ConsoleHost')
     Set-PSReadLineKeyHandler -Key Shift+RightArrow -Function SelectForwardChar
     Set-PSReadLineKeyHandler -Key Ctrl+Shift+LeftArrow -Function SelectBackwardWord
     Set-PSReadLineKeyHandler -Key Ctrl+Shift+RightArrow -Function SelectNextWord
+
+    Set-PSReadlineOption -BellStyle None
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadlineOption -MaximumHistoryCount 10000 -HistoryNoDuplicates 
+    Set-PSReadlineOption -AddToHistoryHandler {
+        param([string]$line)
+        # Do not save any command line unless it has more than 3 characters.  Prevents storing ls, gci, etc.
+        return $line.Length -gt 3
+    }
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+    Set-PSReadlineKeyHandler -Key Ctrl+Shift+C `
+         -BriefDescription CopyPathToClipboard `
+         -LongDescription "Copies the current path to the clipboard" `
+         -ScriptBlock { (Resolve-Path -LiteralPath $pwd).ProviderPath.Trim() | clip }
     
     # Dracula colors for PSReadLine -- works in conhost and wt -- install colors from https://github.com/dracula/powershell
     Set-PSReadlineOption -Color @{
         "Command" = [ConsoleColor]::Green
-        "Parameter" = [ConsoleColor]::Gray
-        "Operator" = [ConsoleColor]::Magenta
-        "Variable" = [ConsoleColor]::White
-        "String" = [ConsoleColor]::Yellow
-        "Number" = [ConsoleColor]::Blue
-        "Type" = [ConsoleColor]::Cyan
         "Comment" = [ConsoleColor]::DarkCyan
+        "ContinuationPrompt" = [ConsoleColor]::White
+        "Default" = [ConsoleColor]::White
+        "Emphasis" = [ConsoleColor]::Cyan
+        "Error" = [ConsoleColor]::Red
+        "InlinePrediction" = [ConsoleColor]::DarkCyan
+        "Keyword" = [ConsoleColor]::Green
+        "Member" = [ConsoleColor]::White
+        "Number" = [ConsoleColor]::Blue
+        "Operator" = [ConsoleColor]::Magenta
+        "Parameter" = [ConsoleColor]::Gray
+        "String" = [ConsoleColor]::Yellow
+        "Type" = [ConsoleColor]::Cyan
+        "Variable" = [ConsoleColor]::White
     }
 
     Import-Module oh-my-posh
-    $DefaultUser = 'wafuqua' # hide "username@host" when on localhost and this user.
-    Set-Prompt
+    Set-PoshPrompt -Theme ~/.oh-my-posh.omp.json
 
-    Import-Module z # track frequently used directories and jump to them with z
+    # track frequently used directories and jump to them with z
+    Import-Module z
 
+    # use the coreutils ls instead of Get-ChildItem
     function ls_alias { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
     Set-Alias -Name ls -Value ls_alias -Option Private
 }
